e955e12b301fc556d131f387104c8369dc0874d3
Joel Gonzaga <joel.gonzaga@usaepay.com>
2017-12-28 15:22:29 -0800

    the character model corrently instantiates now, including the modifiers.

--- src/app/models/character.model.ts
+++ src/app/models/character.model.ts
@@ -3,11 +3,12 @@ export class Character {
     public resources: number;
     public karma: number;
     public attributes: Attributes;
-    /* Further testing ...
-    public conditions: conditions;
+    public conditions: Conditions;
+    /* "qualities" is not yet ready on the back end.
+       Will keep here an will probably do it for real soon
     public qualities: qualities;
     */
-    constructor(basic, attributes, resources, karma) {
+    constructor(basic, attributes, resources, karma, mental, physical) {
         this.basic = new Basic(
             basic.name,
             basic.metatype,
@@ -33,57 +34,57 @@ export class Character {
             attributes.mag,
             attributes.res
         );
+        this.conditions = new Conditions(mental, physical );
+    }
+}
+
+export class Conditions {
+    public physical = {
+        boxes: 0,
+        modifiers: 0
+    };
+    public mental = {
+        boxes: 0,
+        modifiers: 0
+    };
+    getmod(x) {
+        return Math.floor(x / 3) * -1;
+    }
+    constructor(mental, physical) {
+        this.physical.boxes = physical;
+        this.mental.boxes = mental;
+        this.physical.modifiers = this.getmod(physical);
+        this.mental.modifiers = this.getmod(mental);
     }
 }
 
 export class Basic {
-    public name: string;
-    public metatype: string;
-    public age: number;
-    public gender: string;
-    public height: number;
-    public weight: number;
-    public alias: string[];
-    constructor(name, metatype, age, gender, height, weight, alias) {
-        this.name = name || null;
-        this.metatype = metatype || null;
-        this.age = age || null;
-        this.gender = gender || null;
-        this.height = height || null;
-        this.weight = weight || null;
+    constructor(
+        public name: string = null,
+        public metatype: string = null,
+        public age: number = 0,
+        public gender: string = null,
+        public height: number = 0,
+        public weight: number = 0,
+        public alias: string[] = []) {
     }
 }
 
 export class Attributes {
-    /*
-        There is an obvsious reason to use a loop here.
-        Pending stack Overflow to figure that part out.
-    */
-    public bod: number;
-    public agi: number;
-    public rea: number;
-    public str: number;
-    public wil: number;
-    public log: number;
-    public int: number;
-    public cha: number;
-    public edg: number;
-    public ess: number;
-    public ini: number;
-    public mag: number;
-    public res: number;
-    constructor(bod, agi, rea, str, wil, log, int, cha, edg, ess, ini, mag, res){
-        this.bod = bod || 0;
-        this.agi = agi || 0;
-        this.rea = rea || 0;
-        this.wil = wil || 0;
-        this.log = log || 0;
-        this.int = int || 0;
-        this.cha = cha || 0;
-        this.edg = edg || 0;
-        this.ess = ess || 0;
-        this.ini = ini || 0;
-        this.mag = mag || 0;
-        this.res = res || 0;
+    constructor(
+        public bod: number = 0,
+        public agi: number = 0,
+        public rea: number = 0,
+        public str: number = 0,
+        public wil: number = 0,
+        public log: number = 0,
+        public int: number = 0,
+        public cha: number = 0,
+        public edg: number = 0,
+        public ess: number = 0,
+        public ini: number = 0,
+        public mag: number = 0,
+        public res: number = 0
+    ) {
     }
 }
--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -4,7 +4,7 @@ import { Character } from '../models/character.model';
 @Injectable()
 export class CharactersService {
   runTest() {
-    const candid = new Character({ name: 'susan', height: 14 }, { bod: 6 }, 5, 3);
+    const candid = new Character({ name: 'susan', height: 14, alias: ['cindy', 'is0bel'] }, { bod: 6, ess: 6 }, 5, 3, 1, 4);
     console.log(candid);
   }
   constructor() { }
