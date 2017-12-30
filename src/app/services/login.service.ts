68af6be83951f9b1f4eff9d95984b8bf87587e8a
Joel Gonzaga <joel.gonzaga@usaepay.com>
2017-12-29 16:02:29 -0800

    continued to add services and components.

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,9 @@
+/*
+    Will handle Authenticat
+    Will Trigger a "load" of all characters and "user" data from backend
+    Will Communicate with characters and logins service to that end
+*/
+
+export class LoginService {
+
+}
