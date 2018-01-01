4888b279018305e998cb11d7da74cecafbce8a76
Joel Gonzaga <joel.gonzaga@usaepay.com>
2018-01-01 14:54:48 -0800

    added an outline for all the functions the characters service ought to have.

--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -7,8 +7,38 @@ export class CharactersService {
     new Character('101', {name: 'susan', height: 14, alias: ['cindy', 'is0bel'] }, { bod: 6, ess: 6 }, 5, 3, 1, 4),
     new Character('202', {name: 'glory', height: 14, alias: ['gretal', 'your death'] }, { bod: 6, ess: 6 }, 5, 3, 1, 4)
   ];
+
   getRunners() {
     return this.party.slice();
+  }
+
+  loadRunners() {
+    // pulls all charcters from database.
+    // Puts runners into 'party' variable.
+  }
+
+  saveNewRunner(runner) {
+    // From a form, saves a new runner to the database.
+    // All runners are reloaded into private party variable for sake of resyncing.
+  }
+
+  clearRunners() {
+    // In the event of a log out, Private party array is set to an empty array.
+  }
+
+  getOneRunner(id) {
+    // Returns one data based on the ID.
+    // Runner is loaded from this service.
+  }
+
+  patchOneRunner(runner) {
+    // 'Runner' is patched to the database.
+    // Loads single runner into this service from Database
+  }
+
+  loadOneRunner(id) {
+    // grabs one runner from the backend.
+    // Loads into private party array in this service.
   }
   constructor() { }
 
