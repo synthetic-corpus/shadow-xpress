52be1931556e3287f2212000f2848526c1717ac1
Joel Gonzaga <joel.gonzaga@usaepay.com>
2018-01-05 15:13:31 -0800

    qualities services successfully loads everything from the server.

--- src/app/forms/addquality/addquality.component.ts
+++ src/app/forms/addquality/addquality.component.ts
@@ -17,6 +17,8 @@ export class AddqualityComponent implements OnInit {
       'description': new FormControl(null, Validators.required),
       'cost': new FormControl(0, Validators.required)
     });
+    this.qualityService.loadQualities();
+    this.qualityService.loadQualities();
   }
 
   onSubmit() {
--- src/app/services/qualities.service.ts
+++ src/app/services/qualities.service.ts
@@ -17,15 +17,46 @@ export class QualityService {
     extractDocument(response) {
         return response.json().quality;
     }
+
+    documentToQuality(document) {
+        return new Quality(
+            document.name,
+            document.description,
+            document.cost,
+            document._id,
+            document.beneficial
+        );
+    }
+
     addQuality(qualityObject) {
         this.http.post('http://localhost:3000/qualities/new', qualityObject)
             .subscribe(
                 (response) => {
                     const document = this.extractDocument(response);
-                    console.log(document);
+                    this.loadQualities();
                 },
                 (err) => {
                     console.log(err);
                 });
+    }
+    // Loads all the qualities from the databases, and puts them into this services' array
+    loadQualities() {
+        this.http.get('http://localhost:3000/qualities/getall')
+            .subscribe(
+                (response) => {
+                    if (response.json()) {
+                        this.allQualities = [];
+                    }
+                    const array = response.json();
+                    array.forEach((document) => {
+                        const quality = this.documentToQuality(document);
+                        this.allQualities.push(quality);
+                    });
+                    console.log(this.allQualities.length);
+                },
+                (err) => {
+                    console.log(err.json());
+                }
+            );
     }
 }
