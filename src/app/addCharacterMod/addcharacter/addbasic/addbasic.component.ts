e4ee737eec82968210de892fe278f087e3fb139c
Joel Gonzaga <joel.gonzaga@usaepay.com>
2018-01-10 16:55:17 -0800

    router links are working now. Added the basic of the form group for add basic.

--- src/app/addCharacterMod/addcharacter/addbasic/addbasic.component.ts
+++ src/app/addCharacterMod/addcharacter/addbasic/addbasic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-addbasic',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addbasic.component.css']
 })
 export class AddBasicComponent implements OnInit {
+  metas = ['human', 'dwarf', 'elf', 'troll', 'ork' ];
+  genders = ['male', 'female', 'trans', 'non-binary' ];
 
+  basicform: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.basicform = new FormGroup({
+      'name': new FormControl(null),
+      'metatype': new FormControl(null),
+      'gender': new FormControl(null),
+      'height': new FormControl(null),
+      'weight': new FormControl(null),
+    });
   }
 
 }
